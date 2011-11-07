63bce55bf5939956c1ecdf184e7ff24b2f3ae8ad
John Patrick <jp@canyourigit.com>
2011-11-06 20:25:45 -0800

    updated utils

--- utils/PyUtils.py
+++ utils/PyUtils.py
@@ -39,25 +39,4 @@ def isIter(arg, string=False):
         return True
     return False
 
-#json utils
-def _decode_list(lst):
-    newlist = []
-    for i in lst:
-        if isinstance(i, unicode):
-            i = i.encode('utf-8')
-        elif isinstance(i, list):
-            i = _decode_list(i)
-        newlist.append(i)
-    return newlist
 
-def _decode_dict(dct):
-    newdict = {}
-    for k, v in dct.iteritems():
-        if isinstance(k, unicode):
-            k = k.encode('utf-8')
-        if isinstance(v, unicode):
-             v = v.encode('utf-8')
-        elif isinstance(v, list):
-            v = _decode_list(v)
-        newdict[k] = v
-    return newdict

a3e67b43b67bff21341c2ffcd19a7becd52f75e9
JP <jp@canyourigit.com>
2012-07-26 13:39:21 -0700

    added editable control info

--- control.py
+++ control.py
@@ -26,7 +26,6 @@ for f in fncs:
 """
 
 import logging, sys, copy, json, os
-import pymel.core as pm
 import maya.mel as MM
 import maya.cmds as MC
 
@@ -101,10 +100,10 @@ def _argHandleData(**kwargs):
             _logger.debug('skipping invalid handle arg "%s"' % k)
     return result
 
-def _modHandleData(xform, **kwargs):
+# def _modHandleData(xform, **kwargs):
 
-    nodeData = eval(pm.getAttr('%s.%s' % (xform, INFO_ATTR)))
-    return _argHandleData(handleData=nodeData, **kwargs)
+#     nodeData = eval(pm.getAttr('%s.%s' % (xform, INFO_ATTR)))
+#     return _argHandleData(handleData=nodeData, **kwargs)
 
 def _getLibDir():
     return os.path.join(os.path.dirname(__file__), 'control_lib')
@@ -169,14 +168,20 @@ def _setColor(xform, color):
 
 INFO_ATTR = 'beingsControlInfo'
 
-def getInfo(control):
-    return eval(pm.getAttr('%s.%s' % (control, INFO_ATTR)))
+def getInfo(control, includeEdits=True):
+    info =  eval(MC.getAttr('%s.%s' % (control, INFO_ATTR)))
+    editor = getEditor(control)
+    if includeEdits and editor:
+        info['pos'] = list(MC.getAttr('%s.t' % editor)[0])
+        info['rot'] = list(MC.getAttr('%s.r' % editor)[0])
+        info['scale'] = list(MC.getAttr('%s.s' % editor)[0])
+    return info
 
 def setInfo(control, info):
     control = str(control)
     if not MC.attributeQuery(INFO_ATTR, n=control, ex=1):
         MC.addAttr(control, ln=INFO_ATTR, dt='string')
-    if type(info) == dict:
+    if isinstance(info, dict):
         info = repr(info)
     MC.setAttr('%s.%s' % (control, INFO_ATTR), info, type='string')
 
@@ -212,12 +217,7 @@ def makeControl(name, xformType=None, **kwargs):
 
 
     #create an attribute to store handle info
-
-    if not MC.attributeQuery(INFO_ATTR, n=xform, ex=1):
-        MC.addAttr(xform, ln=INFO_ATTR, dt='string')
-
     handleData = _argHandleData(**kwargs)
-    MC.setAttr('%s.%s' % (xform, INFO_ATTR), repr(handleData), type='string')
 
     #snap the tmp shape to the xform
     tmpXform = _importShape(handleData['shape'])
@@ -232,34 +232,45 @@ def makeControl(name, xformType=None, **kwargs):
     utils.parentShape(xform, tmpXform)
     if handleData.get('type') != 'surface':
         _setColor(xform, handleData['color'])
+
+    #set the handle info
+    setInfo(xform, handleData)
+
     return xform
 
-def bbCenter(shape, freeze=True):
-    """
-    Center the shape's bounding box
-    """
-    bb = shape.getBoundingBox()
-    mvX = (bb[1][0] + bb[0][0]) / -2.0
-    mvY = (bb[1][1] + bb[0][1]) / -2.0
-    mvZ = (bb[1][2] + bb[0][2]) / -2.0
-    utils.moveShapePos(shape, vector=[mvX, mvY, mvZ])
-    if freeze:
-        pm.makeIdentity(shape, apply=True, r=0, s=0, t=1, n=0)
 
-def bbScale(shape, freeze=True):
-    """
-    Scale the obj to a bounding box with a scale of 1
-    """
-    bb = shape.getBoundingBox()
-    #get the longest side:
-    sides = [bb[1][0] - bb[0][0],
-             bb[1][1] - bb[0][1],
-             bb[1][2] - bb[0][2]]
+def getEditor(ctl):
+    editor = '%s_editor' % ctl
+    if MC.objExists(editor):
+        return editor
+    return None
 
-    sclAmt = 2.0 / max(sides)
-    pm.scale(shape, [sclAmt, sclAmt, sclAmt], r=True)
-    if freeze:
-        pm.makeIdentity(shape, apply=True, r=0, s=1, t=0, n=0)
+
+def setEditable(ctl, state):
+    """
+    Add a new xform to a control
+    """
+    editor = getEditor(ctl)
+    if editor and state:
+        return False
+    elif not editor and not state:
+        return False
+
+    info  = getInfo(ctl)
+    if state:
+        editor = MC.createNode('transform', name='%s_editor' % ctl, parent=None)
+        MC.parent(editor, ctl)
+        MC.setAttr('%s.t' % editor, *info['pos'], type='double3')
+        MC.setAttr('%s.r' % editor, *info['rot'], type='double3')
+        MC.setAttr('%s.s' % editor, *info['scale'], type='double3')
+
+        utils.parentShape(editor, ctl, deleteChildXform=False)
+
+    else:
+        setInfo(ctl, info)
+        utils.parentShape(ctl, editor, deleteChildXform=True)
+    return True
+
 
 def snapKeepShape(target, ctl, scaleTo1=True, **kwargs):
     """Snap the xform but retain the shape
@@ -419,7 +430,7 @@ def setupFkCtls(bndJnts, oldFkCtls, fkToks, namer):
 class Differ(object):
     """
     Get and set control differences
-    """
+    """    
     def __init__(self):
         self.__controls = {}
         self.__initialState = {}
@@ -541,231 +552,3 @@ class Differ(object):
 
             if localmatrix and xformSpace == 'local':
                 pm.xform(ctl, m=localmatrix)
-
-
-
-
-#####################################################
-## curve shapes
-####################################################
-
-def makeShape_cube():
-
-    """A Cube Curve"""
-    crv = pm.curve(d=1, p=[(-0.5, 0.5, 0.5), (0.5, 0.5, 0.5), (0.5, 0.5, -0.5), (-0.5, 0.5, -0.5), (-0.5, 0.5, 0.5),
-                           (-0.5, -0.5, 0.5), (-0.5, -0.5, -0.5), (0.5, -0.5, -0.5), (0.5, -0.5, 0.5), (-0.5, -0.5, 0.5),
-                           (0.5, -0.5, 0.5), (0.5, 0.5, 0.5), (0.5, 0.5, -0.5), (0.5, -0.5, -0.5), (-0.5, -0.5, -0.5),
-                           (-0.5, 0.5, -0.5)],
-                            k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-
-
-def makeShape_sphere():
-
-    c1 = pm.circle(nr=[1, 0, 0], sw=360, r=1, d=3, s=12)[0]
-    c2 = pm.circle(nr=[0, 1, 0], sw=360, r=1, d=3, s=12)[0]
-    c3 = pm.circle(nr=[0, 0, 1], sw=360, r=1, d=3, s=12)[0]
-
-
-def makeShape_fatCross():
-
-    pm.curve(d=1,
-            p=[(1, 0, -1), (2, 0, -1), (2, 0, 1), (1, 0, 1), (1, 0, 2), (-1, 0, 2), (-1, 0, 1),
-               (-2, 0, 1), (-2, 0, -1), (-1, 0, -1), (-1, 0, -2), (1, 0, -2), (1, 0, -1)],
-               k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-
-def makeShape_diamond():
-    pm.curve(d=1,
-            p=[(0, 0, -2.5), (0, 0, 2.5), (-2.5, 0, 0), (0, 2.5, 0), (2.5, 0, 0), (0, -2.5, 0), (0, 0, -2.5),
-              (2.5, 0, 0), (0, 0, 2.5), (0, 2.5, 0), (0, 0, -2.5), (-2.5, 0, 0), (0, -2.5, 0), (0, 0, 2.5), ],
-              k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],)
-
-
-def makeShape_doublePin():
-    crv = pm.curve(p=[(0, 1.375001, 0), (-0.250001, 1.625001, 0), (0, 1.875, 0), (0.25, 1.625001, 0),
-                      (0, 1.375001, 0), (0, -1.374999, 0), (-0.25, -1.625, 0), (0, -1.875, 0), (0.250001, -1.625, 0), (0, -1.374999, 0)],
-                   k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], d=1)
-    crv.rx.set(90)
-    return crv
-
-def makeShape_lightbulb():
-    """A lightbulb curve"""
-
-    crv = pm.curve(p=[(-0.139471, -0.798108, 0), (-0.139471, -0.798108, 0), (-0.139471, -0.798108, 0), (-0.299681, -0.672294, 0),
-                      (-0.299681, -0.672294, 0), (-0.299681, -0.672294, 0), (-0.121956, -0.578864, 0), (-0.121956, -0.578864, 0), (-0.121956, -0.578864, 0),
-                      (-0.285304, -0.51952, 0), (-0.285304, -0.51952, 0), (-0.0744873, -0.442806, 0), (-0.0744873, -0.442806, 0), (-0.287769, -0.373086, 0),
-                      (-0.287769, -0.373086, 0), (-0.100386, -0.296549, 0), (-0.100386, -0.296549, 0), (-0.264344, -0.205725, 0), (-0.264344, -0.205725, 0),
-                      (-0.262544, -0.0993145, 0), (-0.262544, -0.0993145, 0), (-0.167051, -0.0613459, 0), (-0.167051, -0.0613459, 0),
-                      (-0.167051, -0.0613459, 0), (-0.166024, 0.0163458, 0), (-0.157394, 0.232092, 0), (-0.367902, 0.680843, 0), (-0.96336, 1.224522, 0),
-                      (-1.006509, 1.992577, 0), (-0.316123, 2.613925, 0), (0.561786, 2.548479, 0), (1.094888, 2.001207, 0), (1.051638, 1.166965, 0),
-                      (0.436419, 0.66543, 0), (0.13283, 0.232092, 0), (0.15009, 0.0163458, 0), (0.15073, -0.046628, 0), (0.15073, -0.046628, 0),
-                      (0.270326, -0.0955798, 0), (0.270326, -0.0955798, 0), (0.267815, -0.208156, 0), (0.267815, -0.208156, 0), (0.0884224, -0.291145, 0),
-                      (0.0884224, -0.291145, 0), (0.292477, -0.366091, 0), (0.292477, -0.366091, 0), (0.0946189, -0.439723, 0), (0.0946189, -0.439723, 0),
-                      (0.306664, -0.508968, 0), (0.306664, -0.508968, 0), (0.112488, -0.57513, 0), (0.112488, -0.57513, 0), (0.323789, -0.674644, 0),
-                      (0.323789, -0.674644, 0), (0.152097, -0.794645, 0), (0.152097, -0.794645, 0), (0.152097, -0.794645, 0), (0.106716, -0.907397, 0),
-                      (0.0103741, -1.003739, 0), (-0.0919896, -0.907397, 0), (-0.139471, -0.798108, 0), (-0.139471, -0.798108, 0)],
-                   k=[0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-                      32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 59, 59], d=3)
-    crv.rx.set(-90)
-    return crv
-
-def makeShape_hand():
-
-
-    crv = pm.curve(p=[(0, 0.2, 0.3), (0, 0, 0.2), (0, 0, -0.2), (0, 0.2, -0.3), (0, 0.498495, -0.496524),
-                      (0, 0.580389, -0.361639), (0, 0.36361, -0.207485), (0, 0.599659, -0.173764),
-                      (0, 1.023583, -0.168946), (0, 1.139199, 0.028564), (0, 0.927237, 0.370594), (0, 0.5, 0.3),
-                      (0, 0.2, 0.3)], k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], d=1)
-    return crv
-
-def makeShape_arrow():
-
-    crv = pm.curve(p=[(-0.671879, 1.840622, 1), (-0.671879, 1.840622, 3), (-0.671879, 1.840622, 3),
-                      (-1.582662, 2.55714, 1.178396), (-2.371522, 2.815571, 0), (-2.371522, 2.815571, 0),
-                      (-1.560737, 2.546095, -1.129296), (-0.671879, 1.840622, -3), (-0.671879, 1.840622, -3),
-                      (-0.671879, 1.840622, -1), (-0.671879, 1.840622, -1), (0, 0, -1),
-                      (-0.671879, -1.840622, -1), (-0.671879, -1.840622, -1), (-0.671879, -1.840622, -3),
-                      (-0.671879, -1.840622, -3), (-1.495543, -2.511834, -1.301146), (-2.371522, -2.815571, 0),
-                      (-2.371522, -2.815571, 0), (-1.474011, -2.500042, 1.399345), (-0.671879, -1.840622, 3),
-                      (-0.671879, -1.840622, 3), (-0.671879, -1.840622, 1), (-0.671879, -1.840622, 1), (0, 0, 1),
-                      (-0.671879, 1.840622, 1)],
-                   k=[0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 23, 23], d=3)
-    crv.rx.set(90)
-    crv.rz.set(-90)
-    return crv
-
-def makeShape_text(text="text", font="Arial_Bold"):
-
-    """Make a cuve from text.
-
-    kwargs:
-    text:  the text of the curve ("text")
-    font:  the font of the text  ("Arial_Bold")
-    """
-    try:
-        txtPrnt = pm.PyNode(pm.textCurves(text=text, font=font, ch=0)[0])
-    #on my linux system, there are only super-basic fonts installed.
-    except RuntimeError:
-        txtPrnt = pm.PyNode(pm.textCurves(text=text, font="Courier", ch=0)[0])
-    txtParts = []
-    for txtGrp in txtPrnt.getChildren():
-        for txt in txtGrp.getChildren():
-            txtParts.append(txt)
-            txtParts[-1].setParent(world=True)
-
-    pm.delete(txtPrnt)
-    txt = utils.parentShapes(txtParts[0], txtParts[1:])
-    txt.rx.set(-90)
-    return txt
-
-
-def makeShape_square():
-    crv = pm.curve(p=[(1, 0, 1), (-1, 0, 1), (-1, 0, -1), (1, 0, -1), (1, 0, 1)], k=[0, 1, 2, 3, 4], d=1)
-    return crv
-
-def makeShape_circle():
-    return pm.circle(nr=[0, 1, 0], sw=360, d=3, s=12)[0]
-def makeShape_triangle():
-    return MM.eval('curve -d 1 -p 0 0 1 -p -0.866 0 -0.5 -p 0.866 0 -.5 -p 0 0 1  -k 0 -k 1 -k 2 -k 3 ;')
-
-def makeShape_cross():
-    crv = pm.PyNode(MM.eval('curve -d 1 -p -1 0 -5 -p 1 0 -5 -p 1 0 -1 -p 5 0 -1 -p 5 0 1 -p 1 0 1 -p 1 0 5 -p -1 0 5 -p -1 0 1 -p -5 0 1 -p -5 0 -1 -p -1 0 -1 -p -1 0 -5 -k 0 -k 1 -k 2 -k 3 -k 4 -k 5 -k 6 -k 7 -k 8 -k 9 -k 10 -k 11 -k 12 ;'))
-
-def makeShape_cross3d():
-    crv = pm.PyNode(MM.eval('curve -d 1 -p -1 0 -5 -p 1 0 -5 -p 1 0 -1 -p 5 0 -1 -p 5 0 1 -p 1 0 1 -p 1 0 5 -p -1 0 5 -p -1 0 1 -p -5 0 1 -p -5 0 -1 -p -1 0 -1 -p -1 0 -5 -k 0 -k 1 -k 2 -k 3 -k 4 -k 5 -k 6 -k 7 -k 8 -k 9 -k 10 -k 11 -k 12 ;'))
-    crv.s.set([0.2,0.2,0.2])
-    dup = pm.duplicate(crv)[0]
-    dup.rx.set(90)
-    dup = pm.duplicate(crv)[0]
-    dup.rx.set(90)
-    dup.ry.set(90)
-
-def makeShape_jack():
-    makeShape_doublePin()
-    makeShape_doublePin().ry.set(90)
-    makeShape_doublePin().rx.set(0)
-
-def makeShape_layoutGlobals():
-    pm.circle(normal=[0,1,0], r=2)
-    MM.eval('curve -d 3 -p -1.169488 0 0.463526 -p -0.958738 0 0.971248 -p -0.421459 0 1.196944 -p 0.271129 0 1.255888 -p 0.994735 0 1.004275 -p 1.168226 0 0.417207 -k 0 -k 0 -k 0 -k 1 -k 2 -k 3 -k 3 -k 3 ;')
-    crv = MM.eval('curve -d 3 -p 0.81419 0 -1.322437 -p 1.084855 0 -1.105855 -p 1.19143 0 -0.932901 -p 1.321428 0 -0.734364 -k 0 -k 0 -k 0 -k 1 -k 1 -k 1 ;')
-    pm.duplicate(crv)[0].sx.set(-1)
-    crv = pm.circle(normal=[0,1,0], r=.25, cx=-.77, cz=-.66)[0]
-    pm.duplicate(crv)[0].sx.set(-1)
-
-
-#############################################
-## surface shapes
-#############################################
-
-def makeShape_orbit_srf():
-
-    torus, tShp = pm.torus(heightRatio=0.05)
-    cone1, c1Shp = pm.cone(radius=0.2, axis=[0, 0, 1])
-    MM.eval('nurbsPrimitiveCap 1 1 0')
-    cone2, c2Shp = pm.cone(radius=0.2, axis=[0, 0, 1])
-    MM.eval('nurbsPrimitiveCap 1 1 0')
-    cone1.ty.set(1)
-    cone2.ty.set(-1)
-    cone2.ry.set(180)
-    for item in [cone1, cone2, torus]:
-        pm.delete(item, ch=True)
-    for shp in [tShp, c1Shp, c2Shp]:
-        pass
-        #pm.disconnectAttr(
-    utils.parentShapes(torus, [cone1, cone2])
-    torus.rz.set(90)
-
-
-def makeShape_sphere_srf():
-
-    return pm.sphere()[0]
-
-def makeShape_cube_srf():
-    c = pm.nurbsCube()[0]
-    cb = pm.group(em=True, world=True)
-    utils.parentShapes(cb, c.listRelatives(children=True, pa=True))
-    pm.delete(c)
-    return cb
-
-
-def makeShape_arrow_srf(length=2, coneRadius=1, tailRadius=.5):
-    cone = pm.cone(ax=[0, 1, 0], r=coneRadius)[0]
-    MM.eval('nurbsPrimitiveCap 1 1 0')
-    cone.ty.set(length + 1)
-    cylinder = pm.cylinder(ax=[0, 1, 0], r=tailRadius, hr=float(length) / float(tailRadius))[0]
-    MM.eval('nurbsPrimitiveCap 3 1 1')
-    cylinder.ty.set(float(length) / 2)
-
-
-all_shapes = {}
-for fName, f in sys.modules[__name__].__dict__.items():
-    if fName.startswith('shape_'):
-        tmp, sName, sType = fName.split('_')
-        all_shapes[sName.lower() + sType.capitalize()] = f
-
-
-def getAllShapes():
-    """
-    Return a dictionary of {'shapeName': ShapeFunc}
-    """
-    return all_shapes
-
-def layoutGlobalsNode():
-    """Create a layout globals node"""
-    tmp = pm.ls('being_control')
-    if tmp:
-        return tmp[0]
-
-    ctl = makeControl(shape='layoutGlobals', xformType='transform', name='being_control', color='salmon')
-
-    for attr in ctl.listAttr(keyable=1):
-        try:
-            attr.setLocked(True)
-            attr.setKeyable(False)
-        except:
-            pass
-    ctl.addAttr('layoutControlVis', at='bool', k=0, dv=1)
-    ctl.layoutControlVis.set(cb=1)
-    ctl.addAttr('rigControlVis', at='bool', k=0, dv=1)
-    ctl.rigControlVis.set(cb=1)
-    return ctl
